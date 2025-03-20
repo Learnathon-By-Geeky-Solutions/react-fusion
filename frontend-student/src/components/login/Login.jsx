f0dee1e89818954e81bdcb7c0fce1d03ac90c6af
naim <naim.baijid@gmail.com>
2025-03-20 16:19:14 +0600

    login ui

--- frontend-student/src/components/login/Login.jsx
+++ frontend-student/src/components/login/Login.jsx
@@ -1,3 +1,131 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { FormikProvider, useFormik } from "formik";
+
 export default function Login() {
-  return <>Login Page</>;
+  const handleLogIn = (data) => {
+    console.log("login", data);
+  };
+  const handleSignUp = (data) => {
+    console.log("signup ", data);
+  };
+  const loginFormik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      handleLogIn(values);
+    },
+  });
+
+  const signupFormik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      handleSignUp(values);
+    },
+  });
+  return (
+    <div className="flex align-middle my-12 justify-around">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Log In</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <form onSubmit={loginFormik.handleSubmit}>
+            <Card>
+              <CardHeader>
+                <CardTitle>Log In</CardTitle>
+                <CardDescription>
+                  Enter email password to log into your account
+                </CardDescription>
+              </CardHeader>
+
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    onChange={loginFormik.handleChange("email")}
+                    value={loginFormik.values.email}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    onChange={loginFormik.handleChange("password")}
+                    value={loginFormik.values.password}
+                    type="password"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button type="submit">Save changes</Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </TabsContent>
+
+        <TabsContent value="signup">
+          <form onSubmit={signupFormik.handleSubmit}>
+            <Card>
+              <CardHeader>
+                <CardTitle>Sign Up</CardTitle>
+                <CardDescription>
+                  Enter details to create account
+                </CardDescription>
+              </CardHeader>
+
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="name">Name</Label>
+                  <Input
+                    id="name"
+                    onChange={signupFormik.handleChange("name")}
+                    value={signupFormik.values.name}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    onChange={signupFormik.handleChange("email")}
+                    value={signupFormik.values.email}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    onChange={signupFormik.handleChange("password")}
+                    value={signupFormik.values.password}
+                    type="password"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button type="submit">Save changes</Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
