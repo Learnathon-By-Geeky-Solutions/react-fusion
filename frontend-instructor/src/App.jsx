39387812065377044bb70451c890be93cf74fe41
attoCoderSaim <mahfuzsaim9@gmail.com>
2025-03-26 21:20:49 +0600

    Installed Shadcn and react-router dom

--- frontend-instructor/src/App.jsx
+++ frontend-instructor/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Button } from "@/components/ui/button"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +9,9 @@ function App() {
       <h1 class="text-3xl font-bold underline">
         Instructor Here!
       </h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
