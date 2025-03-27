74b95ee3a69d933a81e669978cb71a4e938bae68
naim <naim.baijid@gmail.com>
2025-03-27 12:50:23 +0600

    use accessValidation middleware

--- backend/src/app/modules/comment/comment.service.ts
+++ backend/src/app/modules/comment/comment.service.ts
@@ -1,63 +1,9 @@
 import { JwtPayload } from "../../../interfaces/common"
 import { ICommentsByVideoId, ICreateComment, IUpdateComment } from "./comment.interface"
 import prisma from "../../../shared/prisma"
-import ApiError from "../../../errors/ApiError"
-import httpStatus from "http-status"
-
-const checkAccess = async (user: JwtPayload, videoId: string) => {
-    const video = await prisma.video.findUnique({
-        where: { id: videoId },
-        include: {
-            module: {
-                include: {
-                    milestone: {
-                        include: {
-                            course: true
-                        }
-                    }
-                }
-            }
-        }
-
-    },)
-    const instructorId = (video?.module.milestone.course.instructorId)
-    const courseId = (video?.module.milestone.course.id)
-    if (user.role === 'INSTRUCTOR' && user.userId === instructorId) {
-        return true
-    }
-
-    const isCourseBought = await prisma.transactions.findFirst({
-        where: {
-            AND: [
-                {
-                    studentId: {
-                        equals: user.userId
-                    }
-                },
-                {
-                    courseId: {
-                        equals: courseId
-                    }
-                }
-            ]
 
 
-        }
-    })
-    if (isCourseBought) {
-        return true
-    }
-    return false
-}
-
-
-const getCommentsByVideoId = async (user: JwtPayload, payload: ICommentsByVideoId) => {
-    const isAuthorized = await checkAccess(user, payload.videoId)
-    if (!isAuthorized) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized')
-
-    }
-
+const getCommentsByVideoId = async (payload: ICommentsByVideoId) => {
     const result = await prisma.comment.findMany({
         where: {
             ...payload,
@@ -67,11 +13,6 @@ const getCommentsByVideoId = async (user: JwtPayload, payload: ICommentsByVideoI
 }
 
 const createComment = async (user: JwtPayload, payload: ICreateComment) => {
-    const isAuthorized = await checkAccess(user, payload.videoId)
-    if (!isAuthorized) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized')
-
-    }
     const result = await prisma.comment.create({
         data: {
             userId: user.userId,
@@ -82,14 +23,9 @@ const createComment = async (user: JwtPayload, payload: ICreateComment) => {
     return result
 }
 
-const updateComment = async (user: JwtPayload, payload: IUpdateComment) => {
-    const comment = await prisma.comment.findUnique({ where: { id: payload.id } })
-    if (comment?.userId !== user.userId) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized')
-    }
-
+const updateComment = async (payload: IUpdateComment) => {
     const result = await prisma.comment.update({
-        where: { id: payload.id },
+        where: { id: payload.commentId },
         data: {
             comment: payload.comment
         }
@@ -97,14 +33,10 @@ const updateComment = async (user: JwtPayload, payload: IUpdateComment) => {
     return result
 }
 
-const deleteComment = async (user: JwtPayload, id: string) => {
-    const comment = await prisma.comment.findUnique({ where: { id: id } })
-    if (comment?.userId !== user.userId) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized')
-    }
+const deleteComment = async (commentId: string) => {
     const result = await prisma.comment.deleteMany({
         where: {
-            id: id
+            id: commentId
         },
     })
     return result
