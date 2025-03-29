3182a8f6df57c5d291efedd3c8ad192d14e9e8a9
naim <naim.baijid@gmail.com>
2025-03-29 22:00:15 +0600

    some components

--- ts-test/src/App.tsx
+++ ts-test/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
+import { CardWithForm } from './Test'
+import { TabsDemo } from './Test2'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='items-center justify-center'>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,17 +21,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button
+          onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </div >
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <CardWithForm></CardWithForm>
+      <br />
+      <TabsDemo></TabsDemo>
+    </div>
   )
 }
 
