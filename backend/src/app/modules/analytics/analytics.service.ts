a1453c32d29915b1b12581db4a9e33b45d56bef4
naim <naim.baijid@gmail.com>
2025-04-05 09:14:34 +0600

    single course analytics

--- backend/src/app/modules/analytics/analytics.service.ts
+++ backend/src/app/modules/analytics/analytics.service.ts
@@ -1,4 +1,3 @@
-import { truncate } from "fs";
 import { JwtPayload } from "../../../interfaces/common";
 import prisma from "../../../shared/prisma";
 
@@ -30,7 +29,7 @@ const getStudentAll = async (user: JwtPayload) => {
 
 
 const getStudentOne = async (user: JwtPayload, courseId: string) => {
-	const result = await prisma.courseProgress.findUnique({
+	const courseProgress = await prisma.courseProgress.findUnique({
 		where: {
 			studentId_courseId: {
 				studentId: user.userId,
@@ -45,6 +44,172 @@ const getStudentOne = async (user: JwtPayload, courseId: string) => {
 		},
 
 	})
+
+
+	const progressId = courseProgress?.id || ""
+
+	const totalVideos = await prisma.video.count({
+		where: {
+			module: {
+				milestone: {
+					courseId: courseId
+				}
+			}
+		}
+	})
+
+	const completedVidoes = await prisma.videoProgress.count({
+		where: {
+			AND: [
+				{
+					courseProgressId: {
+						equals: progressId
+					}
+				},
+				{
+					isCompleted: {
+						equals: true
+					}
+				}
+			]
+		}
+	})
+
+	const totalQuizes = await prisma.quiz.count({
+		where: {
+			module: {
+				milestone: {
+					courseId: courseId
+				}
+			}
+		}
+	})
+
+	const completedQuizes = await prisma.quizProgress.count({
+		where: {
+			AND: [
+				{
+					courseProgressId: {
+						equals: progressId
+					}
+				},
+				{
+					isCompleted: {
+						equals: true
+					}
+				}
+			]
+		}
+	})
+	const totalModules = await prisma.module.count({
+		where: {
+			milestone: {
+				courseId: courseId
+			}
+		}
+	})
+	const completedModules = await prisma.moduleProgress.count({
+		where: {
+			AND: [
+				{
+					courseProgressId: {
+						equals: progressId
+					}
+				},
+				{
+					isCompleted: {
+						equals: true
+					}
+				}
+			]
+		}
+	})
+
+	const totalMilestones = await prisma.milestone.count({
+		where: {
+			courseId: courseId
+		}
+	})
+	const completedMilestones = await prisma.milestoneProgress.count({
+		where: {
+			AND: [
+				{
+					courseProgressId: {
+						equals: progressId
+					}
+				},
+				{
+					isCompleted: {
+						equals: true
+					}
+				}
+			]
+		}
+	})
+
+	const totalScore = await prisma.question.aggregate({
+		where: {
+			quiz: {
+				module: {
+					milestone: {
+						courseId: courseId
+					}
+				}
+			}
+		},
+		_sum: {
+			points: true
+		}
+	})
+	const userScore = await prisma.quizProgress.aggregate({
+		where: {
+			courseProgressId: progressId
+		},
+		_sum: {
+			score: true
+		}
+	})
+
+	const totalWatchTime = await prisma.video.aggregate({
+		where: {
+			module: {
+				milestone: {
+					courseId: courseId
+				}
+			}
+		},
+		_sum: {
+			length: true
+
+		}
+	})
+
+	const userWatchTime = await prisma.videoProgress.aggregate({
+		where: {
+			courseProgressId: progressId
+		},
+		_sum: {
+			timeWatched: true
+		}
+	})
+
+	const result = {
+		courseProgress,
+		summary: {
+			totalVideos: totalVideos,
+			completedVidoes: completedVidoes,
+			totalwatchTime: totalWatchTime._sum.length,
+			userWatchTime: userWatchTime._sum.timeWatched,
+			totalQuizes: totalQuizes,
+			completedQuizes: completedQuizes,
+			totalScore: totalScore._sum.points,
+			userScore: userScore._sum.score,
+			totalModules: totalModules,
+			completedModules: completedModules,
+			totalMilestones: totalMilestones,
+			completedMilestones: completedMilestones,
+		}
+	}
 	return result
 }
 
