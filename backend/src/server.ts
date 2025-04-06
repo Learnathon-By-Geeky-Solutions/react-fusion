654184ec638fe38a2a107e490d7d79f467881360
naim <naim.baijid@gmail.com>
2025-04-06 11:51:23 +0600

    remove commented logerr

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,10 @@
 import { Server } from 'http';
 import app from './app';
 import config from './config';
-// import { errorlogger, logger } from './shared/logger';
 
 
 async function bootstrap() {
   const server: Server = app.listen(config.port, () => {
-    // logger.info(`Server running on port ${config.port}`);
     console.log(`Server running on port ${config.port}`);
 
   });
@@ -14,7 +12,6 @@ async function bootstrap() {
   const exitHandler = () => {
     if (server) {
       server.close(() => {
-        // logger.info('Server closed');
         console.log('Server closed');
       });
     }
@@ -22,7 +19,6 @@ async function bootstrap() {
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    // errorlogger.error(error);
     console.log(error);
 
     exitHandler();
@@ -32,7 +28,6 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
-    // logger.info('SIGTERM received');
     if (server) {
       server.close();
     }
