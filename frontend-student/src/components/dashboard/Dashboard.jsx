e147f1f03bd15150c7dd8802df9d331291ba2ea5
attoCoderSaim <mahfuzsaim9@gmail.com>
2025-04-09 00:31:45 +0600

    Initial setup for dashboard, completed the dashboard service

--- frontend-student/src/components/dashboard/Dashboard.jsx
+++ frontend-student/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import useApi from '@/src/hooks/useApi';
+import { getDashboard, getSingleCourse } from '@/src/services/dashboard';
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+export default function Dashboard() {
+  const [courses, setCourses] = useState([]);
+  const { fetchData } = useApi();
+
+  useEffect(() => {
+    async function fetchCourses() {
+      try {
+        const response = await fetchData(getDashboard, {});
+        if (response.success) {
+          const enrolledCourses = response?.data?.enrolledCourses || [];
+          const courseIds = enrolledCourses.map((course) => course.courseId);
+
+          courseIds.forEach(async (courseId) => {
+            try {
+              const singleCourseRes = await fetchData(
+                getSingleCourse,
+                courseId
+              );
+            } catch (err) {
+              console.error(
+                `Failed to fetch single course for ID ${courseId}:`,
+                err
+              );
+            }
+          });
+
+          setCourses(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    }
+
+    fetchCourses();
+  }, []);
+
+  return <div className='dashboard'></div>;
 }
-
-export default Dashboard
