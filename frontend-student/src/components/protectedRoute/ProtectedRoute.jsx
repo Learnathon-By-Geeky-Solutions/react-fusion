e3f11e2dd88225647723622d4db1d955414ffdb0
attoCoderSaim <mahfuzsaim9@gmail.com>
2025-04-08 09:05:23 +0600

    Made changes on ProtectedRoute

--- frontend-student/src/components/protectedRoute/ProtectedRoute.jsx
+++ frontend-student/src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useAuth from '@/src/context/authContext';
 import PropTypes from 'prop-types';
 
 export default function ProtectedRoute({ children }) {
   const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user.authenticated) {
+      navigate('/login', { replace: true }); 
+    }
+  }, [user, navigate]);
 
   if (!user.authenticated) {
-    return <>NO USER</>;
+    return null;
   }
 
   return <>{children}</>;
