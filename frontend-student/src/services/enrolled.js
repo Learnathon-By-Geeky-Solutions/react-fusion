39f7a12e3ae83aab3051b9a519ee7cdf04b911ae
attoCoderSaim <mahfuzsaim9@gmail.com>
2025-04-09 17:24:01 +0600

    Added Enrolled service to check if an user has enrolled a course

---
+++ frontend-student/src/services/enrolled.js
@@ -0,0 +1,19 @@
+import { BACKEND } from '../constants';
+
+export async function getVideo(payload) {
+  try {
+    const result = await fetch(`${BACKEND}/video/${payload.data.courseId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: payload.user.token,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching video:', error);
+    return { success: false };
+  }
+}
