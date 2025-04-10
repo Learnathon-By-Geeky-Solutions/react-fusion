dfa3b524e5c0dd0b237560677d0400403b706674
attoCoderSaim <mahfuzsaim9@gmail.com>
2025-04-10 19:41:00 +0600

    Added Progress Service

---
+++ frontend-student/src/services/progress.js
@@ -0,0 +1,77 @@
+import { BACKEND } from '../constants';
+
+export async function milestone(payload) {
+  try {
+    const result = await fetch(`${BACKEND}/progress/milestone`, {
+      method: 'POST',
+      headers: {
+        Authorization: payload.user.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload.data)
+    });
+
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Error buying course:', error);
+    return { success: false };
+  }
+}
+
+export async function module(payload) {
+  try {
+    const result = await fetch(`${BACKEND}/progress/module`, {
+      method: 'POST',
+      headers: {
+        Authorization: payload.user.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload.data)
+    });
+
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Error buying course:', error);
+    return { success: false };
+  }
+}
+
+export async function quiz(payload) {
+  try {
+    const result = await fetch(`${BACKEND}/progress/quiz`, {
+      method: 'POST',
+      headers: {
+        Authorization: payload.user.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload.data)
+    });
+
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Error buying course:', error);
+    return { success: false };
+  }
+}
+
+export async function video(payload) {
+  try {
+    const result = await fetch(`${BACKEND}/progress/video`, {
+      method: 'POST',
+      headers: {
+        Authorization: payload.user.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload.data)
+    });
+
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Error buying course:', error);
+    return { success: false };
+  }
+}
