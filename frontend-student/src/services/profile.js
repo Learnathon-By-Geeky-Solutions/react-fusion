5862b360918d0607f1f55add9a9ff5f59932dd3e
attoCoderSaim <mahfuzsaim9@gmail.com>
2025-04-12 18:39:13 +0600

    Completed the profile page

--- frontend-student/src/services/profile.js
+++ frontend-student/src/services/profile.js
@@ -2,19 +2,22 @@ import { BACKEND } from '../constants';
 
 export async function profile(payload) {
   try {
-    const result = await fetch(`${BACKEND}/user/profile`, {
+    const response = await fetch(`${BACKEND}/user/profile`, {
       method: 'GET',
       headers: {
         Authorization: payload.user.token,
         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload.data)
+      }
     });
 
-    const data = await result.json();
+    if (!response.ok) {
+      throw new Error(`Error fetching profile data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Error fetching profile:', error);
-    return { success: false };
+    console.error('Error fetching profile data:', error);
+    return null;
   }
 }
