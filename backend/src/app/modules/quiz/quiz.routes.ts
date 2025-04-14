7b72e1a455d46e202876a19981728fb02be7b06a
naim <naim.baijid@gmail.com>
2025-04-14 17:35:06 +0600

    proper rest

--- backend/src/app/modules/quiz/quiz.routes.ts
+++ backend/src/app/modules/quiz/quiz.routes.ts
@@ -10,11 +10,11 @@ import accessValidation from '../../middlewares/accessValidation';
 const router = express.Router();
 router.get("/:quizId", auth(UserRole.INSTRUCTOR, UserRole.STUDENT), accessValidation('quiz'), quizController.getQuiz)
 
-router.post("/create", auth(UserRole.INSTRUCTOR), validateRequest(quizValidationSchema.quizCreateSchema), accessValidation('module'), quizController.createQuiz)
+router.post("/", auth(UserRole.INSTRUCTOR), validateRequest(quizValidationSchema.quizCreateSchema), accessValidation('module'), quizController.createQuiz)
 
-router.put("/update/:quizId", auth(UserRole.INSTRUCTOR), validateRequest(quizValidationSchema.quizUpdateSchema), accessValidation('quiz'), quizController.updateQuiz)
+router.put("/:quizId", auth(UserRole.INSTRUCTOR), validateRequest(quizValidationSchema.quizUpdateSchema), accessValidation('quiz'), quizController.updateQuiz)
 
-router.delete("/delete/:quizId", auth(UserRole.INSTRUCTOR), accessValidation('quiz'), quizController.deleteQuiz)
+router.delete("/:quizId", auth(UserRole.INSTRUCTOR), accessValidation('quiz'), quizController.deleteQuiz)
 
 router.post("/check/:quizId", auth(UserRole.STUDENT), validateRequest(quizValidationSchema.quizCheckingSchema), accessValidation('quiz'), quizController.checkQuiz)
 
